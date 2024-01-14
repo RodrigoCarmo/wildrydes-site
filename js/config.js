161bf750547a2dc8457caf2ae885a28165dbf7ca
Rodrigo Carmo <roodr@Rodrigo.localdomain>
2024-01-14 10:23:49 -0300

    chore: altera config para cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_LL6LKUVXn', 
+        userPoolClientId: '3aec1glmp26gs4gt9boi0v61bh', 
+        region: 'us-east-1' 
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
